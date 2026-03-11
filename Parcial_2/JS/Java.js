3a2828a25db8fe6358fa30124331ae21211700cb
GioLiH <giovanni25105@gmail.com>
2026-03-10 21:32:34 -0600

    Ejercicio ColorEvento

---
+++ Parcial_2/JS/Java.js
@@ -0,0 +1,16 @@
+/* 
+Lenguaje de programacion ligero interpretado, orientado a objetos y basado en prototipos. Es un lenguaje de programación que se utiliza principalmente para el desarrollo web, aunque también se puede usar para crear aplicaciones de escritorio y móviles. JavaScript es un lenguaje de programación muy popular y ampliamente utilizado en la industria del desarrollo web, y es compatible con la mayoría de los navegadores web modernos. Con JavaScript, los desarrolladores pueden crear sitios web interactivos y dinámicos, así como aplicaciones web complejas y sofisticadas.
+Todos lo soportan los nav web
+
+lo usan angular, react frameworks
+nodejs para el backend
+nosotros usaremos Vanilla JS   es decir puro no usando frameworks
+
+JavaScript es un lenguaje de programación basado en prototipos, multiparadigma, de un solo hilo, dinámico, con soporte para programación orientada a objetos, imperativa y declarativa.
+Generalmente es utilizado por múltiples librerías de web frontend como Angular o React. 
+
+
+node es para el lado back --> server
+browser es para el lado front --> cliente
+*/
+
---
+++ Parcial_2/JS/script.js
@@ -0,0 +1,10 @@
+/*
+function saludar(){
+        const a =2;
+        const b =3;
+        const c = a + b;
+        console.log(c);
+        alert("Hola mundo desde una funcion");
+       
+    }
+*/
