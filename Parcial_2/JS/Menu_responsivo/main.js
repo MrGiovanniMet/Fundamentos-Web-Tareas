e02ec4c4d41164df598c92b5be76c206c8a4d124
GioLiH <giovanni25105@gmail.com>
2026-03-19 19:44:20 -0600

    Ejercicios P2

---
+++ Parcial_2/JS/Menu_responsivo/main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("nav");
+function toogleMenu()
+{
+    // if(menubar.classList.contains("open"))
+    // {
+    //     menubar.classList.remove("open")
+    // }
+    // else{
+    //     menubar.classList.add("open")
+    // }
+
+    nav.classList.toggle("open");
+}
